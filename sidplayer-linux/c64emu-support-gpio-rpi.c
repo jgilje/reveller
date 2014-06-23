0040ecdd5cd82d358a3b674d0746f000e6fc1abe
Joakim L. Gilje <jgilje@jgilje.net>
2014-06-23 08:03:42 +0200

    only clear the pins we use to interface SID

--- sidplayer-linux/c64emu-support-gpio-rpi.c
+++ sidplayer-linux/c64emu-support-gpio-rpi.c
@@ -14,8 +14,8 @@ void c64_sid_block_end(void) {
 
 void c64_sid_write(uint8_t reg, uint8_t data) {
 	uint8_t data_01 = data & 0x3;
-	uint8_t data_23 = data & 0xC;
-	uint8_t data_4567 = data & 0xF0;
+	uint8_t data_23 = (data & 0xC) >> 2;
+	uint8_t data_4567 = (data & 0xF0) >> 4;
 	
 	uint32_t set_pins = 0;
 	set_pins |= data_01;		// compatability with v1
@@ -23,7 +23,6 @@ void c64_sid_write(uint8_t reg, uint8_t data) {
 	set_pins |= data_23 << 14;
 	set_pins |= data_4567 << 22;
 
-	set_pins |= (1 << 4);		// SID CS
 	set_pins |= (reg & 0x1F) << 7;	// SID Address
 	
 	// SET Pins
@@ -31,17 +30,17 @@ void c64_sid_write(uint8_t reg, uint8_t data) {
 	
 	// Clear Pins
 	uint32_t clear_pins = 0;
-	clear_pins |= (1 << 17);	// SID Write
+	clear_pins |= (1 << 17);	// SID read/Write
+	clear_pins |= (1 << 4);		// SID CS
 	*bcm2835_registers.gpio_output_clear0 = clear_pins;
 	
-	usleep(1000);
+	usleep(1);
 	
-	clear_pins = 0xFFFDFFFF;	// Clear all, except
-	set_pins = (1 << 17);		// SID Read
+	clear_pins = 0x03C0CF8F;	// Clear 0-3, 7-11, 14-15, 22-25
+	set_pins  = (1 << 17);		// SID Read/write
+	set_pins |= (1 << 4);		// SID CS
 	*bcm2835_registers.gpio_output_set0 = set_pins;
-	usleep(1000);
 	*bcm2835_registers.gpio_output_clear0 = clear_pins;
-	return;
 }
 
 void c64_set_freq_vic(uint32_t hz) {
