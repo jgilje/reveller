0c4c84ec4a4e1d4a3f71d1f8b8bf08df8bbaa74a
megatrond <trond@trondfoto.no>
2015-05-22 23:36:38 +0200

    hooked up reveller react component to its root element

--- sidplayer-frontend/app/app.js
+++ sidplayer-frontend/app/app.js
@@ -1,7 +1,16 @@
 const React = require('react');
-
 const SidplayerController = require('./controllers/SidplayerController');
-
 const sidplayer = new SidplayerController('ws://localhost:8080/ws');
 
+class RevellerApp extends React.Component {
+	constructor() {
+		super();
+	}
+	render() {
+		return (
+			<div className="yolo">lolorama</div>
+		)
+	}
+}
 
+React.render(<RevellerApp/>, document.getElementById('reveller'));
