40e9461e87f61dce7b431634f5d162dd2e8bff03
tmolberg <tmolberg@gmail.com>
2022-09-10 20:52:13 +0200

    Delete sidplayer-frontend directory
    No longer being developed. Swapped out for new frontend..

--- sidplayer-frontend/app/constants/RevellerConstants.js
+++
@@ -1,9 +0,0 @@
-const keymirror = require('keymirror');
-
-export default {
-	ActionTypes: keymirror({
-		GET_DIRECTORY: null,
-		LOADING_DIRECTORY: null,
-		RECEIVE_DIRECTORY: null
-	})
-}
--- sidplayer-frontend/app/dispatcher/AppDispatcher.js
+++
@@ -1,3 +0,0 @@
-const AppDispatcher = require('flux').Dispatcher;
-
-module.exports = new AppDispatcher();
