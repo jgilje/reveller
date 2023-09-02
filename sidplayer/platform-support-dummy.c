40118d82eb353e1820bdd72c6fe57b5a2833f442
Joakim L. Gilje <jgilje@jgilje.net>
2023-09-02 13:59:41 +0200

    added dummy implementations of reveller_platform power and shutdown

--- sidplayer/platform-support-dummy.c
+++ sidplayer/platform-support-dummy.c
@@ -1,9 +1,9 @@
-#include "platform-support.h"
 #include "platform-support-common.h"
+#include "platform-support.h"
 
 #include <stdarg.h>
-#include <stdio.h>
 #include <stdint.h>
+#include <stdio.h>
 #include <stdlib.h>
 
 #ifdef unix
@@ -16,18 +16,20 @@
 
 static void dummy_usleep(uint32_t us) {
 #if defined unix
-	usleep(us);
+  usleep(us);
 #elif defined _WIN32
-	Sleep(us/1000);
+  Sleep(us / 1000);
 #endif
 }
 
 static void dummy_sid_write(uint8_t reg, uint8_t data) {
-//	platform_debug("SID Write: %x: %x\n", reg, data);
+  //	platform_debug("SID Write: %x: %x\n", reg, data);
 }
 
-static void init() {
-}
+static void dummy_power() {}
+static void dummy_shutdown(uint32_t state) {}
+
+static void init() {}
 
 struct reveller_platform dummy_platform = {
     .init = &init,
@@ -38,6 +40,8 @@ struct reveller_platform dummy_platform = {
     .pause = &common_pause,
     .resume = &common_resume,
     .usleep = &dummy_usleep,
+    .power = &dummy_power,
+    .shutdown = &dummy_shutdown,
 
     .read = &common_platform_read_source,
 
